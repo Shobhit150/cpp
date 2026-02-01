485fec0d3ceb322b2f2015d0e175c6bd32d84049
Shobhit150 <vshobhit150@gmail.com>
2026-02-01 21:55:13 +0530

    done

--- dp/Striver/PartitionDP/BurstBalloons.cpp
+++ dp/Striver/PartitionDP/BurstBalloons.cpp
@@ -4,6 +4,39 @@
 using namespace std;
 class Solution {
 public:
+    int memo(vector<int>& nums, int i, int j, vector<vector<int>> &dp) {
+        if(i>j) return 0;
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int ans = INT_MIN;
+        for(int k=i;k<=j;k++) {
+            
+            int cost = bruteForce(nums, i, k-1) +
+                        bruteForce(nums, k+1, j) +
+                        nums[i-1] * nums[k] * nums[j+1];
+            ans = max(ans, cost);
+        }
+        return dp[i][j] = ans;
+    }
+    int tab(vector<int>& nums) {
+        int m = nums.size();
+        vector<vector<int>> dp(m, vector<int>(m, 0));
+        for(int i=m-2;i>=1;i--) {
+            for(int j=i;j<=m-2;j++) {
+                int ans = INT_MIN;
+                for(int k=i;k<=j;k++) {
+                    
+                    int cost = dp[i][k-1] +
+                                dp[k+1][j] +
+                                nums[i-1] * nums[k] * nums[j+1];
+                    ans = max(ans, cost);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[1][m-2];
+    }
     int bruteForce(vector<int>& nums, int i, int j) {
         if(i>j) return 0;
         int ans = INT_MIN;
@@ -11,7 +44,7 @@ public:
             
             int cost = bruteForce(nums, i, k-1) +
                         bruteForce(nums, k+1, j) +
-                        nums[k-1] * nums[k] * nums[k+1];
+                        nums[i-1] * nums[k] * nums[j+1];
             ans = max(ans, cost);
         }
         return ans;
@@ -22,7 +55,8 @@ public:
         for(int &x: nums) num.push_back(x);
         num.push_back(1);
         int m = num.size();
-        return bruteForce(num, 1, m-2);
+        vector<vector<int>> dp(m, vector<int>(m, -1));
+        return tab(num);
     }
 };
 
--- dp/Striver/PartitionDP/DifferentWaystoEvaluateaBooleanExpression.cpp
+++ dp/Striver/PartitionDP/DifferentWaystoEvaluateaBooleanExpression.cpp
@@ -4,30 +4,20 @@
 using namespace std;
 class Solution {
 public:
-    int bruteForce(vector<int>& nums, int i, int j) {
-        if(i>j) return 0;
-        int ans = INT_MIN;
-        for(int k=i;k<=j;k++) {
-            
-            int cost = bruteForce(nums, i, k-1) +
-                        bruteForce(nums, k+1, j) +
-                        nums[k-1] * nums[k] * nums[k+1];
-            ans = max(ans, cost);
+    int bruteForce(string& s, int i, int j) {
+        int count = 0;
+        for(int k=i;k<=j;k+=2) {
+            int left
         }
-        return ans;
     }
-    int maxCoins(vector<int>& nums) {
-        vector<int> num;
-        num.push_back(1);
-        for(int &x: nums) num.push_back(x);
-        num.push_back(1);
-        int m = num.size();
-        return bruteForce(num, 1, m-2);
+    int countTrue(string s) {
+
     }
 };
 
+
 int main(){
     Solution s;
-    vector<int> nums = {3,1,5,8};
-    cout << s.maxCoins(nums) << endl;
+    string expr = "T|T&F^T";
+    cout << s.countTrue(expr) << endl;
 }
